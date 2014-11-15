3a5d32d17bfb53f80749c7364de565b78620c517
Nikolaos Korasidis <renelvon@gmail.com>
2014-11-16 00:44:56 +0200

    Refactor of db_store tests.
    * Handle setup and teardown inside class.
    * Rename file to match module.
    * Use a tempfile rather than a hardoced file.

--- tests/test_db_store.py
+++ tests/test_db_store.py
@@ -1,32 +1,37 @@
-#!/usr/bin/env python
 import os
+import tempfile
 import unittest
 
 from node.db_store import Obdb
-from node.setup_db import setup_db
-
-TEST_DB_PATH = "test/test_ob.db"
-
-
-def setUpModule():
-    # Create a test db.
-    if not os.path.isfile(TEST_DB_PATH):
-        print "Creating test db: %s" % TEST_DB_PATH
-        setup_db(TEST_DB_PATH)
-
-
-def tearDownModule():
-    # Cleanup.
-    print "Cleaning up."
-    os.remove(TEST_DB_PATH)
+from node import setup_db
 
 
 class TestDbOperations(unittest.TestCase):
+    """Test DB operations in an unencrypted DB."""
+
+    @classmethod
+    def setup_path(cls):
+        cls.db_dir = tempfile.mkdtemp()
+        cls.db_path = os.path.join(cls.db_dir, 'testdb.db')
+
+    @classmethod
+    def setup_db(cls):
+        setup_db.setup_db(cls.db_path)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.setup_path()
+        cls.setup_db()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.db_path)
+        os.rmdir(cls.db_dir)
+
+    def setUp(self):
+        self.db = Obdb(self.db_path)
+
     def test_insert_select_operations(self):
-
-        # Initialize our db instance
-        db = Obdb(TEST_DB_PATH)
-
         # Create a dictionary of a random review
         review_to_store = {"pubKey": "123",
                            "subject": "A review",
@@ -35,10 +40,10 @@ class TestDbOperations(unittest.TestCase):
                            "rating": 10}
 
         # Use the insert operation to add it to the db
-        db.insertEntry("reviews", review_to_store)
+        self.db.insertEntry("reviews", review_to_store)
 
         # Try to retrieve the record we just added based on the pubkey
-        retrieved_review = db.selectEntries("reviews", {"pubkey": "123"})
+        retrieved_review = self.db.selectEntries("reviews", {"pubkey": "123"})
 
         # The above statement will return a list with all the
         # retrieved records as dictionaries
@@ -75,10 +80,10 @@ class TestDbOperations(unittest.TestCase):
                            "rating": 10}
 
         # Use the insert operation to add it to the db
-        db.insertEntry("reviews", review_to_store)
+        self.db.insertEntry("reviews", review_to_store)
 
         # Try to retrieve the record we just added based on the pubkey
-        retrieved_review = db.selectEntries("reviews", {"pubkey": "321"})
+        retrieved_review = self.db.selectEntries("reviews", {"pubkey": "321"})
 
         # The above statement will return a list with all the
         # retrieved records as dictionaries
@@ -108,11 +113,11 @@ class TestDbOperations(unittest.TestCase):
         )
 
         # By ommiting the second parameter, we are retrieving all reviews
-        all_reviews = db.selectEntries("reviews")
+        all_reviews = self.db.selectEntries("reviews")
         self.assertEqual(len(all_reviews), 2)
 
         # Use the <> operator. This should return the review with pubKey 123.
-        retrieved_review = db.selectEntries(
+        retrieved_review = self.db.selectEntries(
             "reviews",
             {"pubkey": {"value": "321", "sign": "<>"}}
         )
@@ -124,45 +129,36 @@ class TestDbOperations(unittest.TestCase):
         )
 
     def test_get_or_create_record_when_not_exists(self):
-        db = Obdb(TEST_DB_PATH)
         record = {"city": "Zurich"}
         table = "settings"
-        retrieved_record = db.getOrCreate(table, record)
+        retrieved_record = self.db.getOrCreate(table, record)
         self.assertEqual(retrieved_record["city"], record["city"])
         # check that the missing fields were created as empty
         self.assertEqual(retrieved_record["countryCode"], "")
 
     def test_update_operation(self):
-
-        # Initialize our db instance
-        db = Obdb(TEST_DB_PATH)
-
         # Retrieve the record with pubkey equal to '123'
-        retrieved_review = db.selectEntries("reviews", {"pubkey": "321"})[0]
+        retrieved_review = self.db.selectEntries("reviews", {"pubkey": "321"})[0]
 
         # Check that the rating is still '10' as expected
         self.assertEqual(retrieved_review["rating"], 10)
 
         # Update the record with pubkey equal to '123'
         # and lower its rating to 9
-        db.updateEntries("reviews", {"rating": 9}, {"pubkey": "123"})
+        self.db.updateEntries("reviews", {"rating": 9}, {"pubkey": "123"})
 
         # Retrieve the same record again
-        retrieved_review = db.selectEntries("reviews", {"pubkey": "123"})[0]
+        retrieved_review = self.db.selectEntries("reviews", {"pubkey": "123"})[0]
 
         # Test that the rating has been updated succesfully
         self.assertEqual(retrieved_review["rating"], 9)
 
     def test_delete_operation(self):
-
-        # Initialize our db instance
-        db = Obdb(TEST_DB_PATH)
-
         # Delete the entry with pubkey equal to '123'
-        db.deleteEntries("reviews", {"pubkey": "123"})
+        self.db.deleteEntries("reviews", {"pubkey": "123"})
 
         # Looking for this record with will bring nothing
-        retrieved_review = db.selectEntries("reviews", {"pubkey": "123"})
+        retrieved_review = self.db.selectEntries("reviews", {"pubkey": "123"})
         self.assertEqual(len(retrieved_review), 0)
 
 if __name__ == '__main__':
