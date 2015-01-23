ed3e0cbacd3c8904288ee0674cb4f7d8b4e6c539
maxdebruijn <max.de.bruijn@gmail.com>
2015-01-23 22:57:55 +0100

    Fixed duplicate peers appearing

--- html/controllers/market.js
+++ html/controllers/market.js
@@ -243,16 +243,20 @@ angular.module('app')
                 });
             };
 
+            // Check if peer is already known by comparing the public key
             $scope.add_peer = function(msg) {
-                /* get index if peer is already known */
-                var index = [-1].concat($scope.peers).reduce(
-                    function(previousValue, currentValue, index, array) {
-                        return currentValue.uri == msg.uri ? index : previousValue;
-                    });
+                var alreadyExists = false;
+                angular.forEach($scope.peers,function(peer,index){
+                    if(peer.pubkey === msg.pubkey){
+                        alreadyExists = true;
+                    }
+                });
 
-                if (index == -1) {
-                    /* it is a new peer */
+                if(!alreadyExists){
                     $scope.peers.push(msg);
+                }
+                else{
+                    console.log('Avoided duplicate peer');
                 }
             };
 
