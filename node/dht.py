83bb71e1935b8508d8ed903c9339fe5b8b5acfea
Brian Hoffman <brianchoffman@gmail.com>
2015-03-15 22:50:50 -0400

    Remove mediation request

--- node/dht.py
+++ node/dht.py
@@ -72,6 +72,8 @@ class DHT(object):
                 del self.active_peers[i]
         self.routing_table.remove_contact(guid)
 
+        del self.transport.mediation_mode[guid]
+
         # Refresh GUI peer list
         if self.transport.handler:
             self.transport.handler.refresh_peers()
