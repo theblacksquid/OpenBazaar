7b5f65cc7eda6eb28a07ab39ea45ea63431db7f6
Brian Hoffman <brianchoffman@gmail.com>
2015-04-02 10:26:56 -0400

    Symmetric NAT Tweak

--- node/dht.py
+++ node/dht.py
@@ -97,7 +97,7 @@ class DHT(object):
             if peer.guid == guid:
 
                 # Check if hostname/port combo changed
-                if nat_type != 'Symmetric NAT' and (hostname != peer.hostname or port != peer.port):
+                if (hostname != peer.hostname or port != peer.port):
                     peer.hostname = hostname
                     peer.port = port
                     peer.nat_type = nat_type
